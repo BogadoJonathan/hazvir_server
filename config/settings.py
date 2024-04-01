7813e02695421a1ba3001dce83e3a6de8f8ef97c
jonathan bogado <jb@bb.vision>
2024-04-01 00:24:47 -0300

    lineas eliminadas

--- config/settings.py
+++ config/settings.py
@@ -5,11 +5,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_path = Path('.') / '.env'
-if env_path.exists():
-    load_dotenv(dotenv_path=env_path)
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
