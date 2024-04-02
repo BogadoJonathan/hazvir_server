7089c63ee916238a879bde591a7adcc8c0e1bca9
jonathan bogado <jb@bb.vision>
2024-04-02 01:42:53 -0300

    update urls.py

--- hazvir/urls.py
+++ hazvir/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-from .views import PlayerView, GameView
+from .views import PlayerView, GameView, PlayerInGameView
 
 router_hazvir = routers.DefaultRouter()
 
 router_hazvir.register('players',PlayerView,'get-players')
 router_hazvir.register('games',GameView,'get-games')
+router_hazvir.register('players-in-game',PlayerInGameView,'get-players-in-game')
 
 urlpatterns = router_hazvir.urls
