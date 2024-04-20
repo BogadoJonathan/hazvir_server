62216ef1dfe0f35e2d9722a13763ec51f39ea2a5
jonathan bogado <jb@bb.vision>
2024-04-20 19:31:40 -0300

    update views.py

--- granConexion/views.py
+++ granConexion/views.py
@@ -23,13 +23,13 @@ class VotoDelPublicoView(ModelViewSet):
             return Response({'error': 'Ya has votado en las últimas 12 horas.'}, status=status.HTTP_400_BAD_REQUEST)
         #el VotoDelPublico tiene asociado el modelo: Votacion, donde ahi se muestra a las personas a quien se pueda votar
         #por lo que se debe hacer una validacion para que el voto sea valido, recibiremos el id de la votacion, la buscamos y verificamos el voto
-        votacion_id = request.data.get('Votacion', None)
+        votacion_id = request.data.get('votacion', None)
         votacion = Votacion.objects.get(pk=votacion_id)
         if not votacion:
             return Response({'error': 'La votación no existe.'}, status=status.HTTP_400_BAD_REQUEST)
         if votacion.finaliza < timezone.now():
             return Response({'error': 'La votación ha finalizado.'}, status=status.HTTP_400_BAD_REQUEST)
-        id_player = request.data.get('Player', None)
+        id_player = request.data.get('player', None)
         if not votacion.Nominados.filter(pk=id_player).exists():
             return Response({'error': 'El jugador no está en la votación.'}, status=status.HTTP_400_BAD_REQUEST)
         
