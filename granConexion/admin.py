9c4a2d7d20b961c985e610f1e38ee4f031497653
jonathan bogado <jb@bb.vision>
2024-04-21 12:32:20 -0300

    test admin.py - app: granConexion

--- granConexion/admin.py
+++ granConexion/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import  VotoDelPublico, Votacion
 
 # Register your models here.
-admin.site.register(Votacion)
+class VotoDelPublicoInline(admin.TabularInline):
+    model = VotoDelPublico
+    extra = 0
+
+class VotacionAdmin(admin.ModelAdmin):
+    inlines = [VotoDelPublicoInline]
+    readonly_fields = ['ver_resultados']
+
+    def ver_resultados(self, obj):
+        resultados = obj.obtener_resultados()
+        resultados_html = '<h3>Resultados de la votación:</h3>'
+        for jugador, votos in resultados:
+            resultados_html += f'<p>{jugador}: {votos} votos</p>'
+        return resultados_html
+
+    ver_resultados.short_description = 'Resultados'
+
+admin.site.register(Votacion, VotacionAdmin)
 admin.site.register(VotoDelPublico)
