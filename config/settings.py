8fd09718f4a11db889b9ffa0a08894cd90ee6e9c
jonathan bogado <jb@bb.vision>
2024-04-21 23:04:20 -0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:9000'
+    'http://localhost:9000',
+    '*'
+    
 ]
 
 CORS_ALLOW_METHODS = [
